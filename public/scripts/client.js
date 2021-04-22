79389df4d79c82bb3f9ce268881dcf0c7b609088
Harp Sandhu <happacode@protonmail.com>
2021-04-22 16:22:26 -0700

    Add tweet box layout and timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+/* eslint-env jquery */
+/* eslint-env es6 */
+/* eslint-disable */
+
+$(document).ready(function(timeago) {
+
+
+  
+});
+
+
+console.log(timeago.format(1619025180007));
+
+const date = timeago.format (find object date value here)
